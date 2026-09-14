c31a4fcb4d7f2a1725cc6f9a38493c5cdba35abf
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2026-09-14 17:25:29 -0500

    feat: Add database creation and deletion statements to glob_gusters.sql

--- glob-gusters-videoclub/sql/glob_gusters.sql
+++ glob-gusters-videoclub/sql/glob_gusters.sql
@@ -3,4 +3,8 @@
 -- Fecha: 2024-06-10 
 
 -- Creación de la base de datos
---katalina te amo demasiado 
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `glob_gusters`;
+
+-- Eliminación de la base de datos
+DROP DATABASE IF EXISTS `glob_gusters`;
