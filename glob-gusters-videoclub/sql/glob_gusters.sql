c1cabedd81677f3195b96d6ec2bf7c91329f5926
Katalina Fajardo <katalinafajardo.04@gmail.com>
2026-09-21 17:05:11 -0500

    feat: Add table definitions for Nacionalidad, Actor, Director, and Pelicula in glob_gusters.sql

--- glob-gusters-videoclub/sql/glob_gusters.sql
+++ glob-gusters-videoclub/sql/glob_gusters.sql
@@ -7,4 +7,35 @@ CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLA
 USE `glob_gusters`;
 
 -- Eliminación de la base de datos
-DROP DATABASE IF EXISTS `glob_gusters`;
+--DROP DATABASE IF EXISTS `glob_gusters`;
+
+
+CREATE TABLE IF NOT EXISTS Nacionalidad (
+  NacionalidadID int PRIMARY KEY,
+  Nombre varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Actor (
+  ActorID int PRIMARY KEY,
+  Nombre varchar(100) NOT NULL,
+  Sexo varchar(20) NOT NULL,
+  NacionalidadID int,
+  FOREIGN KEY (NacionalidadID) REFERENCES Nacionalidad(NacionalidadID)
+);
+
+CREATE TABLE IF NOT EXISTS Director (
+  DirectorID int PRIMARY KEY,
+  Nombre varchar(100) NOT NULL,
+  NacionalidadID int,
+  FOREIGN KEY (NacionalidadID) REFERENCES Nacionalidad(NacionalidadID)
+);
+
+CREATE TABLE IF NOT EXISTS Pelicula (
+    PeliculaID int PRIMARY KEY,
+    ProductoraID int(100) NOT NULL,
+    NacionalidadID int,
+    Titulo varchar(100) NOT NULL,
+    DirectorID int,
+    Fecha DATE,
+    FOREIGN KEY (DirectorID) REFERENCES Director(DirectorID)
+);
